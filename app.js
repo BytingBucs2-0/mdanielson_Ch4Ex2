e00ed04c25079a15325994c083082c56ddc38e77
Matt Danielson <Matt Danielson>
2017-02-08 18:55:34 -0500

    added comment variable from input

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var main = function () {
+var main = function () 
+{
 	"use strict";
 	
-	$(".comment-input button").on("click", function (event) {
-		$(".comments").append("<p>this is a new comment</p>");
+	$(".comment-input button").on("click", function (event) 
+	{
+		var $new_comment = $("<p>"),
+			comment_text = $(".comment-input input").val();
+			
+		// var $new_comment = $("<p>").text($(comment_text = $(".comment-input input").val()); // single line version
+		
+		$new_comment.text(comment_text);
+		
+		$(".comments").append($new_comment);		
 	});
 };
 
